d94757900375cd75ed03e8a0b0220b7316dea02f
Ameya Keskar <55844298+Code0Steak@users.noreply.github.com>
2021-03-01 22:05:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,36 @@
 <html>
 <body>
 <h1>PHP page hosted on Heroku</h1>
+  <div class="form">
+
+   
+    
+        
+        <br>
+        <input type="text" placeholder = "Recipe By" name = "cook" value = "">
+        <br>
+        
+        <input type="text" placeholder = "Email" name = "email" value = "">
+        <br>
+        
+        <input type="text" placeholder = "Name of the Pizza" name = "name" value = "">
+        <br>
+        
+        <input type="text" placeholder = "Ingredients" name = "ingr" value = "">
+        <br>
+       
+        <textarea  id="" cols="30" rows="10" name = "method" >
+        
+        </textarea>
+        <br>
+        <div><input type="submit" id = "submit" name = "submit"></div>
+        
+
+  
+
+</div>
 <?php
-echo "This PHP page was developed by Aliasgar Palgharwala for CC Exp5";
+echo "This PHP page was developed by Ameya Keskar for CC Exp5";
 ?>
 </html>
 </body>
